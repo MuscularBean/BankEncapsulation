f6f569f97574d3352fde558597b179a10932aa05
MuscuarBean <email@example.com>
2023-11-19 05:39:59 -0700

    Exercise Complete

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace BankEncapsulation
+{
+
+    /*Create a new public class named BankAccount
+    In your BankAccount class, create the following:
+        A private field of type double named ** balance ** with a value of 0
+        Define a method named Deposit that will accept a double and store that value in the balance field
+        Define a method named GetBalance that will return the amount stored in the balance field*/
+    public class BankAccount
+    {
+        public BankAccount() { }    
+
+        private double balance = 0; 
+
+        public void Deposit(double amount)
+        {
+            balance = amount;
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+        
+
+
+
+        
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace BankEncapsulation
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace BankEncapsulation
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("How much would you like to deposit?");
+            
+            var deposit = double.Parse(Console.ReadLine());
+            
+            BankAccount yourAccount = new BankAccount();
+
+            yourAccount.Deposit(deposit);
+
+            Console.WriteLine($"Thank you! Your balance is {yourAccount.GetBalance()}");
+
+            
+
         }
     }
 }
